a120ea8fbca380c6610e918fdb4768bc47ad7d9e
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2023-12-31 21:16:22 +0800

    4-2

---
+++ python/CoreBook/4/4-2.py
@@ -0,0 +1,12 @@
+#4-2-2
+#exercise 4.1
+pizzas = ['a', 'b', 'c']
+for pizza in pizzas:
+    print(f'I like {pizza} pizza')
+print('I really love pizza!\n')
+
+#exercise 4.2
+animals = ['dog', 'cat', 'bird']
+for animal in animals:
+    print(f'A {animal} would make a great pet.')
+print('Any of these animals would make a great pet')
